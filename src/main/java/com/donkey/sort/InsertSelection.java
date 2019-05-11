749563c6167f4210a98d0c630b59992e13332429
lxincao <lxincao@creditdev.com>
2019-05-11 11:49:51 +0800

    加入了插入排序的时空分析注释

--- src/main/java/com/donkey/sort/InsertSelection.java
+++ src/main/java/com/donkey/sort/InsertSelection.java
@@ -3,7 +3,9 @@ package com.donkey.sort;
 /**
  * @author: lxc
  * @Date: 2019/5/11 10:29
- * @Description:
+ * @Description: 插入排序，算林称最亲民的排序算法，插入排序采用最简单的插入方式对一个整数数组进行排序。它循环数组中从第二个开始的所有元素，
+ * 并且将每一个循环到的元素插入到相应的位置，从而实现排序的目的。
+ * 时间复杂度O(n2) 最好时间复杂度O(n)
  */
 public class InsertSelection {
 
